9d7451a3e8d425022f04a704bfa1442e63b5f3fd
Hendrik Belitz <hendrik@hendrikbelitz.de@>
2020-06-08 17:56:18 +0200

    Example actor systems

---
+++ examples/pingpong.ts
@@ -0,0 +1,67 @@
+import unionize, { UnionOf } from "unionize";
+import { Actor } from "../src/Actor";
+import { ActorRef } from "../src/ActorRef";
+import { ActorSystem } from "../src/ActorSystem";
+import Winston from "winston";
+import { any } from "ramda";
+
+const logger = Winston.createLogger({
+    format: Winston.format.combine(
+        Winston.format.colorize(),
+        Winston.format.timestamp(),
+        Winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+    ),
+    transports: [new Winston.transports.Console()]
+});
+
+const Actions = unionize({
+    Ping: {},
+    Pong: {}
+}, {value: "command"});
+
+type Action = UnionOf<typeof Actions>;
+
+class PingPongActor extends Actor {
+    private counter = 0;
+
+    constructor(name: string, actorSystem: ActorSystem, other: ActorRef) {
+        super(name, actorSystem);
+        if (other) {
+            this.send(other, Actions.Ping());
+        }
+    }
+
+    public async receive(from: ActorRef, message: Action): Promise<void> {
+        Actions.match(message, {
+            "Ping": () => {
+                this.counter++;
+                if (this.counter > 5) {
+                    this.shutdown();
+                    return;
+                }
+                logger.info(this.name +  ": PING");
+                this.send(from, Actions.Pong());
+            },
+            "Pong": () => {
+                this.counter++;
+                logger.info(this.name +  ": PONG");
+                this.send(from, Actions.Ping());
+            },
+        });
+    }
+}
+
+const system = new ActorSystem(logger);
+system.createActor(PingPongActor, {name: "one"});
+system.createActor(PingPongActor, {name: "two"}, system.getActorRef("actors://system/one").orUndefined());
+
+const shutdownHook = () => {
+    const children = system.childrenOf(system.getActorRef("actors://system").orUndefined());
+    if (any(c => c.isShutdown, children)) {
+        system.shutdown();
+    } else {
+        setTimeout(shutdownHook, 500);
+    }
+};
+
+setTimeout(shutdownHook, 500);
---
+++ examples/storageActor.ts
@@ -0,0 +1,68 @@
+import { Maybe, maybe } from "tsmonads";
+import unionize, { UnionOf, ofType } from "unionize";
+import { Actor } from "../src/Actor";
+import { ActorRef } from "../src/ActorRef";
+import { ActorSystem } from "../src/ActorSystem";
+import Winston from "winston";
+
+const logger = Winston.createLogger({
+    format: Winston.format.combine(
+        Winston.format.colorize(),
+        Winston.format.timestamp(),
+        Winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+    ),
+    transports: [new Winston.transports.Console()]
+});
+
+const Actions = unionize({
+    Append: ofType<number>(),
+    Pop: {},
+    Empty: {},
+}, {value: "command"});
+
+type Action = UnionOf<typeof Actions>;
+
+class StorageActor extends Actor {
+    private store: number[] = [];
+
+    constructor(name: string, actorSystem: ActorSystem) {
+        super(name, actorSystem);
+    }
+
+    public async receive(_from: ActorRef, message: Action): Promise<Maybe<number> | void | boolean> {
+        const result = Actions.match<Maybe<number> | void | boolean>(message, {
+            Pop: () => {
+                const [head, ...rest] = this.store;
+                this.store = rest;
+                return maybe(head);
+            },
+            Empty: () => {
+                return this.store.length === 0;
+            },
+            Append: (x) => {
+                this.store.push(x);
+            },
+        });
+        return result;
+    }
+}
+
+
+const getVals = async () => {
+    console.log(await system.ask(storage, Actions.Empty()));
+    console.log(await system.ask(storage, Actions.Pop()));
+    console.log(await system.ask(storage, Actions.Empty()));
+    console.log(await system.ask(storage, Actions.Pop()));
+    console.log(await system.ask(storage, Actions.Empty()));
+    console.log(await system.ask(storage, Actions.Pop()));
+    console.log(await system.ask(storage, Actions.Empty()));
+    console.log(await system.ask(storage, Actions.Pop()));
+    system.shutdown();
+};
+
+const system = new ActorSystem(logger);
+const storage = system.createActor(StorageActor, {name: "storage"});
+system.send(storage, Actions.Append(10));
+system.send(storage, Actions.Append(20));
+system.send(storage, Actions.Append(30));
+getVals();
