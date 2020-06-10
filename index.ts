7b488dab695340b4092977af62ec6370af3c1780
Hendrik Belitz <hendrik@hendrikbelitz.de@>
2020-06-10 17:18:58 +0200

    Build lib

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { Actor } from "./src/Actor";
+export { ActorOptions } from "./src/ActorOptions";
+export { ActorRef } from "./src/ActorRef";
+export { ActorSystem } from "./src/ActorSystem";
+export { SupervisionStrategy } from "./src/SupervisionStrategy";
